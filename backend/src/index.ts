d85e9cb7df2796d6c3caf5585497e202c4877398
Clayton Rafael <clayton.rssouza@gmail.com>
2023-12-20 18:55:28 -0300

    feat: create initial backend

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor rodando!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
