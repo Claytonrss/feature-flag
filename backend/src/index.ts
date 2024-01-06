40fdfd44e29971e3829f761438727bdc472dab09
Clayton Rafael <clayton.rssouza@gmail.com>
2024-01-06 14:51:37 -0300

    refactor: :recycle: improve structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,53 +1,32 @@
 import express from "express";
+import cors from "cors";
+import { router as featureFlagsRouter } from "./routes/featureFlags";
 import { openDb } from "./database/database";
+import * as dotenv from "dotenv";
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+const envFile = process.env.NODE_ENV === "development" ? ".env.dev" : ".env";
+dotenv.config({ path: envFile });
 
+if (!process.env.ALLOWED_ORIGINS) {
+  throw new Error(
+    "A variável ALLOWED_ORIGINS não está definida no arquivo .env"
+  );
+}
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
 app.use(express.json());
+app.use("/feature-flags", featureFlagsRouter);
 
 openDb().then(() => {
   app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
-  });
-
-  app.get("/", (req, res) => {
-    res.send("Servidor rodando!");
-  });
-
-  app.get("/feature-flags", async (req, res) => {
-    try {
-      const db = await openDb();
-      const flags = await db.all("SELECT * FROM feature_flags");
-      res.json(flags);
-    } catch (error: any) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-
-  app.post("/feature-flags", async (req, res) => {
-    const { name, status } = req.body;
-
-    if (name === undefined || status === undefined) {
-      return res
-        .status(400)
-        .json({ error: "Nome e estado da flag são obrigatórios" });
-    }
-
-    try {
-      const db = await openDb();
-      const result = await db.run(
-        "UPDATE feature_flags SET status = ? WHERE name = ?",
-        [status, name]
-      );
-
-      if (result.changes === 0) {
-        return res.status(404).json({ error: "Feature flag não encontrada" });
-      }
-
-      res.status(200).json({ message: "Feature flag atualizada com sucesso" });
-    } catch (error: any) {
-      res.status(500).json({ error: error.message });
-    }
   });
 });
